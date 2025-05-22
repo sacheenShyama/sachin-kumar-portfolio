c5cd4f5955a31997f76401478125fc975ee5c3ce
sachin kumar <dev.sachin207@gmail.com>
2025-05-22 19:25:16 +0530

    project setuped

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main>
+      <div>
+        <h1>Welcome to my 3D Portfolio!</h1>
+      </div>
+    </main>
+  );
+};
+
+export default App;
