9f647224bd7cc520ebf21e8d259b60b34793b815
sachin kumar <dev.sachin207@gmail.com>
2025-06-01 07:01:33 +0530

    experience section added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
 import NavBar from "./components/NavBar";
+import FeatureCards from "./sections/FeatureCards";
+import ExperienceSection from "./sections/ExperienceSection";
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <NavBar />
       <Hero />
       <ShowcaseSection />
+      <FeatureCards />
+      <ExperienceSection />
     </>
   );
 };
