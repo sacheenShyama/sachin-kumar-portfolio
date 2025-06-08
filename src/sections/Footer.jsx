9bee047c20b41e14cfd9e6480b0c8c998bdaad77
sachin kumar <dev.sachin207@gmail.com>
2025-06-08 13:40:10 +0530

    footer card integrated

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { socialImgs } from "../constants";
+import { div } from "three/tsl";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="flex flex-col justify-center">
+          <p>Terms & Conditions</p>
+        </div>
+        <div className="socials">
+          {socialImgs.map((socialImg, index) => (
+            <div key={index} className="icon">
+              <img src={socialImg.imgPath} alt="social icon" />
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-col justify-center">
+          <p className="text-center md:text-end">
+            © {new Date().getFullYear()} Sachin kumar. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
