6c4693704996fb216a45dc85bae9f602eedf29a3
sachin kumar <dev.sachin207@gmail.com>
2025-06-08 13:32:24 +0530

    contact form implemented

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import ShowcaseSection from "./sections/ShowcaseSection";
 import NavBar from "./components/NavBar";
 import FeatureCards from "./sections/FeatureCards";
 import ExperienceSection from "./sections/ExperienceSection";
+import TechStack from "./sections/TechStack";
+import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
 
 const App = () => {
   return (
@@ -13,6 +16,9 @@ const App = () => {
       <ShowcaseSection />
       <FeatureCards />
       <ExperienceSection />
+      <TechStack />
+      <Contact />
+      <Footer />
     </>
   );
 };
---
+++ src/sections/TechStack.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import TitleHeader from "../components/TitleHeader";
+
+const TechStack = () => {
+  return <div>
+<TitleHeader title={"belo is my super power"} sub={"Tech stacks"} />
+
+  </div>;
+};
+
+export default TechStack;
