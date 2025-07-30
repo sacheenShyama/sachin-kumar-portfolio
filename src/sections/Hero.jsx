30c2985d900ee489fc94f0633a108fcc2ced2783
Sachin kumar <93372997+sacheenShyama@users.noreply.github.com>
2025-07-30 10:39:53 +0530

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -61,7 +61,7 @@ const Hero = () => {
         {/* Right: 3D model */}
         <figure>
           <div className="hero-3d-layout ">
-            <HeroExperience />
+{/*             <HeroExperience /> */}
           </div>
         </figure>
       </div>
